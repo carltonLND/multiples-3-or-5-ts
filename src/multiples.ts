1837ddf5f12bca9e607712bc9a8566ec6c8e2ae0
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-06-19 16:03:54 +0100

    Minor change to account for endNum under 3

--- src/multiples.ts
+++ src/multiples.ts
@@ -1,10 +1,10 @@
 function multiples(endNum: number): number {
-  if (endNum <= 0) {
+  if (endNum <= 2) {
     return 0;
   }
 
   let sum: number = 0;
-  for (let num = 1; num < endNum; num++) {
+  for (let num = 3; num < endNum; num++) {
     if (num % 3 === 0 || num % 5 === 0) {
       sum += num;
     }
